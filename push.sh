86c59f036046e8aef3319c6084a894f4d6fa228e
Jordan Huffaker <jordhuff@gmail.com>
2017-03-11 02:39:37 -0500

    forgot to switch one of the branches.

--- push.sh
+++ push.sh
@@ -13,7 +13,7 @@ commit_website_files() {
 
 upload_files() {
   git remote add origin-pages https://${GH_TOKEN}@github.com/colter5991/rtl-architect.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages gh-pages 
+  git push --quiet --set-upstream origin-pages $BRANCH
 }
 
 setup_git
