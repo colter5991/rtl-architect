54ddb9bd418b79b54da884ba15693052e2f95aa8
Jordan Huffaker <JusticeWarrior@users.noreply.github.com>
2017-03-11 02:09:15 -0500

    Update and rename push.sh to .travis/push.sh

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
+  git config --global user.email "jordhuff@gmail.com"
+  git config --global user.name "Justice Warrior"
 }
 
 commit_website_files() {
-  git checkout -b gh-pages
+  git checkout -b $BRANCH
   git add rtl-architect/dist
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
