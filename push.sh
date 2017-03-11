288a5160a4f61fd6cd6e2e48ad5bced544eab741
Jordan Huffaker <jordhuff@gmail.com>
2017-03-11 02:27:53 -0500

    Think this should do it!

--- push.sh
+++ push.sh
@@ -12,7 +12,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
+  git remote add origin-pages https://${GH_TOKEN}@github.com/colter5991/rtl-architect.git > /dev/null 2>&1
   git push --quiet --set-upstream origin-pages gh-pages 
 }
 
