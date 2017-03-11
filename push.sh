85bc417b42a3a8d75f483cf6703c0fbf29716681
Jordan Huffaker <jordhuff@gmail.com>
2017-03-11 02:53:20 -0500

    Testing pushing back to the same branch.

--- push.sh
+++ push.sh
@@ -6,14 +6,13 @@ setup_git() {
 }
 
 commit_website_files() {
-  git checkout -b gh-pages
   git add rtl-architect/dist
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
   git remote add origin-pages https://${GH_TOKEN}@github.com/colter5991/rtl-architect.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages gh-pages
+  git push --quiet --set-upstream origin-pages $BRANCH
 }
 
 setup_git
