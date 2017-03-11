60ca89634caec0ecbd65c96dfef32ae68de70e59
Jordan Huffaker <jordhuff@gmail.com>
2017-03-11 03:02:07 -0500

    Back to the original strategy

--- push.sh
+++ push.sh
@@ -6,13 +6,14 @@ setup_git() {
 }
 
 commit_website_files() {
+  git checkout -b gh-pages
   git add rtl-architect/dist
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin https://${GH_TOKEN}@github.com/colter5991/rtl-architect.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin $BRANCH
+  git remote add origin-pages https://${GH_TOKEN}@github.com/colter5991/rtl-architect.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-pages gh-pages
 }
 
 setup_git
